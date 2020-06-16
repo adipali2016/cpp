17e95d8bc451fcbe48b803e264a488c91d443a48
Ayush Singh <03ayushsingh@gmail.com>
2020-06-16 15:55:32 +0530

    OOPs Member Nesting and Binary Digits Transformation

--- ahoy.cpp
+++ ahoy.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 class binary{
-	private:
+	private://All members are private till specified public
 	string s;
 	void chk_bin(void);
 	public:
@@ -16,14 +16,14 @@ class binary{
 void binary :: read(void){
 	cout<<"Enter a Binary Number"<<endl;
 	cin>>s;
-	chk_bin();
+	chk_bin();//Member Nesting
 }
 void binary :: chk_bin(void){
 	for (int i = 0; i < s.length(); i++)
 	{
 		if (s.at(i)!='0' && s.at(i)!='1'){
 			cout<<endl<<"This is not Binary"<<endl;
-			exit(0);		
+			exit(0);//Use stdlib.h for this, It will terminate the whole programme.
 		}
 	}
 	
@@ -56,7 +56,7 @@ void binary :: display(void){
 int main(){
 	binary b;
 	b.read();
-	// b.chk_bin();
+	// b.chk_bin(); It can't be called
 	b.display();
 	b.ones();
 	b.display();
