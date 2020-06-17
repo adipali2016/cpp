c93a23360485c78e3ce1fae4871e68f7681e27ea
Ayush Singh <03ayushsingh@gmail.com>
2020-06-17 12:31:10 +0530

    Static Methods/Static member functions.

--- ahoy.cpp
+++ ahoy.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 /*Static Data Membeber Variable are property of class and all the  objects of the class share it.
  Also they are sustained till program is executed.*/
+//Static Member function can access only static variables.
+
 class Employee
 {
 	int id;
@@ -19,19 +21,33 @@ public:
 	{
 		cout << "The ID of Employee is " << id << " And Number of Employees are " << count << endl;
 	}
+	// Static Member Functions/ Methods
+	static void getCount(void){
+		// cnt<<id; IT will not work!
+		cout<<"The value of count is "<<count<<endl;
+	}
 	
 };
 
 int Employee ::count; // Default Value is 0. And if it has to be changed it should be changed here(Outside class) not inside class.
-//Count is the static Data member of Employee class
+//Count is the static Data Member of the Employee class
 int main()
 {
 	Employee ayush, tan, mallu;
 	ayush.setData();
 	ayush.getData();
+	
+	Employee::getCount(); //Static Member functions are called through class Name.
+
 	tan.setData();
 	tan.getData();
+
+	Employee::getCount();
+	
 	mallu.setData();
 	mallu.getData();
+	
+	Employee::getCount();
+	
 	return 0;
 }
