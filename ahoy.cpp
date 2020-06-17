244a7292bc438a26ba02699bf3af2084cdd73fc1
Ayush Singh <03ayushsingh@gmail.com>
2020-06-17 18:04:12 +0530

    Friend Function Intro

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
 using namespace std;
-//Passing of Objects as Function Arguments
+/* Friend Functions - 
+	1.Not in the scope of class/ Not a member of class.
+	2.Can't be called from the object of the respective class.
+	3. Can be called without object
+	4. Usually contains objects as arguments.
+	5. Can be declared inside public as well as private section on the class.	
+	6. It can't access the members directly by their names/ object name/member name. 	
+*/ 
+class complex
+{
+private:
+	int a, b;
+public:
+	
+	friend complex sumComlex(complex , complex);//It means that this non member function is allowed to acess my private member.
 
-class complex{
-	int a;
-	int b;
-	public:
-	void setData(int v1, int v2){
-		a = v1;
-		b = v2;
+	void setNumber(int n1,int n2){
+		a = n1;
+		b = n2;
+	}
+	void printNumber(void){
+		cout<<"Your Number is "<<a<<" + "<<b<<"i"<<endl;
 	}
 
-	void setDataBySum(complex o1,complex o2){
-		a = o1.a + o2.a;
-		b = o1.b + o2.b;
-	}
-	void print(void){
-		cout<<"Your complex number is "<<a<<"+"<<b<<"i"<<endl;
-	}
 };
 
+complex sumComlex(complex o1, complex o2){
+	complex o3;
+	o3.setNumber(o1.a + o2.a,o1.b+o2.b);
+	return o3;
+}
+
+
 int main(){
-	complex c1, c2, c3;
-	c1.setData(1,2);
-	c1.print();
-	c2.setData(2,3);
-	c2.print();
-	
-	c3.setDataBySum(c1,c2);
-	c3.print();
-	
+	complex c1,c2,sum;
+	c1.setNumber(1,4);
+	c1.printNumber();	
+	c2.setNumber(5,8);
+	c2.printNumber();	
+
+	sum = sumComlex(c1,c2);
+	sum.printNumber();
 	return 0;
 }
