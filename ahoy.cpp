7aaf6d30ab87c3bed19ab0023b727a1389612747
Ayush Singh <03ayushsingh@gmail.com>
2020-06-18 00:02:57 +0530

    Friend Function Example 1

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,65 +1,31 @@
 #include <iostream>
 using namespace std;
-
-class complex; //Forward Delcaration <---Declaring That Compiler will get the respective class in future.--->
-
-//Sequence of arrangement of classes is important!
-class Calculator
-{
-public:
-	int add(int a, int b)
-	{
-		return a + b;
-	}
-	int sumReal(complex, complex);
-	int sumImg(complex, complex);//Declaring only as object members aren't available yet.
+class Y;
+class X{
+	int data;
+	public:
+		void setValue(int value){
+			data = value;
+		}
+	friend void add(X ,Y);
 };
-class complex
-{
-private:
-	int a, b;
 
-public:
-/*
-<-=========================Friend Function of Other Classes===================->
-//Accessing individual functions as friend.
-	friend int Calculator ::sumReal(complex o1, complex o2);
-	friend int Calculator ::sumImg(complex o1, complex o2);
-*/
-//<=========================Friend Class====================>
-// Allowing whole class to access private data of present Class.
-	friend class Calculator;
-	void setNumber(int n1, int n2)
-	{
-		a = n1;
-		b = n2;
-	}
-
-	void printNumber(void)
-	{
-		cout << "Your Number is " << a << " + " << b << "i" << endl;
-	}
+class Y{
+	int num;
+	public:
+		void setValue(int value){
+			num = value;
+		}
+	friend void add(X ,Y);
 };
-//Defining after class as object members are available here.
-int Calculator ::sumReal(complex o1, complex o2)
-{
-	return (o1.a + o2.a);
+void add(X o1, Y o2){
+	cout<<"The sum of data of X and Y is "<<o1.data+o2.num<<endl;
 }
-int Calculator ::sumImg(complex o1, complex o2)
-{
-	return (o1.b + o2.b);
-}
-
-int main()
-{
-	complex o1, o2;
-	o1.setNumber(1, 4);
-	o2.setNumber(5, 7);
-	Calculator calc;
-	int res = calc.sumReal(o1, o2);
-	cout << "The sum of real part of o1 and o2 is " << res<<endl;
-	
-	int res1 = calc.sumImg(o1, o2);
-	cout << "The sum of Imaginary part of o1 and o2 is " << res1;
+int main(){
+	X a;
+	Y b;
+	a.setValue(3);
+	b.setValue(5);
+	add(a,b);
 	return 0;
 }
