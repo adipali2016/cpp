88f6ac75281aa7bca9fbb8e6143ea4f305b12c21
Ayush Singh <03ayushsingh@gmail.com>
2020-06-18 20:48:38 +0530

    Parameterized  Constructors

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
 using namespace std;
-//<--==================  Constructors Intro  ==================-->
-// Properties   
-//It is a function and has same name as of class.   
-// IT is automatically invoked when a class is created.   
-// They do not have Return type and cannot return values.!   
-// It can have default arguments.   
-// WE cannot refer to their address.   
+
 class Complex
 {
 	int a, b;
 
 public:
-	//Creating a Constructor. 
-	Complex(void); //Constructor Declaration --> In public section Only <--
+	
+	Complex(int, int); 
 	void printData(void)
 	{
 		cout << "The value of a and b is " << a << " + " << b << "i" << endl;
 	}
 };
 
-Complex ::Complex(void)// Default constructors -- as it takes no parameters/arguments.
+Complex ::Complex(int x, int y)//Parameterised Constructor as it takes 2 parameters.
 {
-	a = 10;
-	b = 2;
-	cout<<"Hello World"<<endl;
+	a = x;
+	b = y;
+	// cout<<"Hello World"<<endl;
 }
 
 int main()
 {
-	Complex c1, c2, c3;
+	Complex c1(11, 12);// ----Implicit Call
 	c1.printData();
-	c2.printData();
-	c3.printData();
+	Complex b = Complex(2, 1);// -----Explicit Call
+	b.printData();
 	return 0;
 }
