bbb9866318da8cb5e7b7da7c0a2067e12c46a679
Ayush Singh <03ayushsingh@gmail.com>
2020-06-19 15:41:58 +0530

    Destructors

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,43 +1,36 @@
 #include <iostream>
 using namespace std;
-//Copy Constructors
-//======> All Compilers automatically creates Copy COnstructors if no constructor is found!!
-class Number
+//Destructors - Never takes argument and never return values.
+//When Compiler realizes that there is no need of objects, it destroys it. 
+int count = 0;
+class num
 {
-    int a;
-
+private:
+    
 public:
-    Number()
-    {
-        a = 0;
-    }//Default COnstructors - Must!
-    Number(int num)
-    {
-        a = num;
+    num(){
+        count++;
+        cout<<"This is a time when constructor is called for Object Number "<<count<<endl;
     }
 
-    Number(Number &obj)//Copy Constructor - Not Necessary to create - Read Header comment.
-    {
-        cout<<"Copy constructor called!!!"<<endl;
-        a = obj.a;
+    ~num(){
+        cout<<"Destructor called for object number "<<count--<<endl;
     }
-
-    void Display(void)
-    {
-        cout << "The Number is " << a << endl;
-    }
+    
 };
 
-int main()
-{
-    Number x,y,z(11),z2;
-    x.Display();
-    y.Display();
-    z.Display();
-    // z1(copy constructors) - It should exactly resemble like other objects eg. x or y or z.
-    Number z1(z);
-    z1.Display();
-    // (if) z2 = z; It won't invoke copy constructor.Because z2 object was predefined.
-    // (if) Number z3 = z; It will invoke copy constructor as Object was simultaneously defined and assigned.
+
+int main(){
+    cout<<"We are inside our main function."<<endl;
+    cout<<"Creating 1st object  n1"<<endl;
+    num n1;
+    {
+        cout<<"Entering this block."<<endl;
+        cout<<"Creating 2 more Objects "<<endl;
+        num n2,n3;
+        
+        
+    }
+    cout<<"Back to main"<<endl;
     return 0;
 }
