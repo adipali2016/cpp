ae5b0f1f7ee05985ba2019ab9745e47141168620
Ayush Singh <03ayushsingh@gmail.com>
2020-06-20 22:50:20 +0530

    Protected Access Modifier

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,57 +1,28 @@
 #include <iostream>
 using namespace std;
-//<==================== Single Inheritance =======================>
+
 class Base
 {
-    int data1;
-
-public:
-    int data2;
-    void setData();
-    int getData1();
-    int getData2();
+protected:// Can Be Inherited.
+    int a;
+private:
+    int b;
 };
-
-void Base ::setData()
+/* <-======================== Members Inheritance  =====================->
+   _________________________________________________________________________________
+                | Public Derivation    |   Private Derivation | Protected Derivation|
+    Protected   |Protected             |    Private           | Protected           |
+    Private     |No Inheritance        |    No Inheritance    | No Inheritance      |
+    Public      |Public                |    Private           | Protected           |
+   _________________________________________________________________________________|
+*/
+class Derieved :  Base
 {
-    data1 = 11;
-    data2 = 22;
-}
-int Base ::getData1()
-{
-    return data1;
-}
-
-int Base ::getData2()
-{
-    return data2;
-}
-
-class Derieved : public Base
-{
-    int data3;
-
-public:
-    void process();
-    void display();
 };
-
-void Derieved ::process()
-{
-    data3 = data2 * getData1();
-}
-
-void Derieved ::display()
-{
-    cout << "value of Data 1 is " << getData1() << endl;
-    cout << "value of Data 2 is " << data2 << endl;
-    cout << "value of Data 3 is " << data3 << endl;
-}
 int main()
 {
-    Derieved der;
-    der.setData();
-    der.process();
-    der.display();
+    Base b;
+    Derieved d;
+    cout<<"AHoy World"<<endl;
     return 0;
 }
