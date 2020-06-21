dbdbba3435559cf86dccd80c620938afded575ed
Ayush Singh <03ayushsingh@gmail.com>
2020-06-21 15:22:20 +0530

    Testing

--- ahoy.cpp
+++ ahoy.cpp
@@ -8,7 +8,7 @@ class Base1
     void greet()
     {
         cout<<"Ahoy, How are you?"<<endl;
-	cout<<"This is to test the code on kali machine 2.0"<<endl;
+	cout<<"This is to test the code on kali machine 3.0"<<endl;
     }    
 
 };
