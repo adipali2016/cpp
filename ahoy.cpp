8a93eb39c59bba58f8d11c489abbc28886f0d6d5
Ayush Singh <03ayushsingh@gmail.com>
2020-06-22 23:51:44 +0530

    Initialization List in Constructors

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,61 +1,28 @@
 #include <iostream>
 using namespace std;
-//Use of constructors in Inheritance====->
-class Base1
+//Initialization List in Constructors
+/*
+Syntax - 
+constructoe(argument) : initialization-section
+
 {
-    int data1;
+         body
+}
+*/
+
+class Test
+{
+    int a, b;//Declaration order matters
 
 public:
-    Base1(int a)
+    // Test(int i, int j) : a(i), b(a+j)
+    Test(int i, int j) : b(j), a(b+i) // Attentiom Here !!!!
     {
-        data1 = a;
-        cout << "Base1 class Constructor called" << endl;
-    }
-    void printData1()
-    {
-        cout << "The value of data is " << data1 << endl;
+        cout << "A"<< endl<<a<<endl<<b<< endl;
     }
 };
-class Base2
-{
-    int data2;
-
-public:
-    Base2(int a)
-    {
-        data2 = a;
-        cout << "Base2 class Constructor called" << endl;
-    }
-    void printData2()
-    {
-        cout << "The value of data is " << data2 << endl;
-    }
-};
-
-class Derieved : public Base2, public Base1
-{
-    int der1, der2;
-
-public:
-    Derieved(int a, int b, int c, int d) : Base1(a), Base2(b)
-    {
-        der1 = c;
-        der2 = d;
-        cout << "Derieved called Constructor called " << endl;
-    }
-    void printDataD()
-    {
-        cout << "The value of Derievd1 is " << der1 << endl;
-        cout << "The value of Derievd2 is " << der2 << endl;
-    }
-};
-
 int main()
 {
-    Derieved D(1, 2, 3, 4);
-    D.printData1();
-    D.printData2();
-    D.printDataD();
-
+    Test T(1,1);
     return 0;
 }
