22ba4b8712cec65e2f43ef31aa88428c8792a2a5
Ayush Singh <19u01045@iiitbhopal.ac.in>
2020-07-26 19:36:34 +0530

    Late binding, Run time polymorphism

--- ahoy.cpp
+++ ahoy.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
 using namespace std;
-// this pointers
-class A
+//Base class's pointer can be used by derieved class pointer
+//But if any inherited function is called through pointer it will call base class pointer only!
+//run-time polymorphism
+class BaseClass
 {
-    int a;
-    public:
-        // A& setData(int a)
-        void setData(int a)
-        {
-          this->a = a;// this - is a keyword which is a pointer which points to the object which invokes the member function. In other words || (A* this) = (this)
-        //   return *this;
-        }
-        void getData()
-        {
-            cout<<"The value of a is "<<a<<endl;
-        }
+public:
+    int varbase;
+    void display()
+    {
+        cout << "This is a Base class variable " << varbase << endl;
+    }
 };
 
-int main(){
-    A b;
-    // b.setData(4).getData();
-    b.setData(4);
-    b.getData();
-    
+class DerievedClass : public BaseClass
+{
+public:
+    int varderieved;
+    void display()
+    {
+        cout << "This is a Base class variable " << varderieved << endl;
+    }
+};
+int main()
+{
+    BaseClass * base_pointer;
+    BaseClass obj_base;
+    DerievedClass obj_derieved;
+    base_pointer = &obj_derieved; //Base class pointer pointing to derieved class pointer
+    base_pointer->varbase = 55;
+    // base_pointer->varderieved = 5; // it will through error 
+    base_pointer->display(); // late binding - it will run base class function
+
+    DerievedClass * derieved = &obj_derieved;
+    derieved->varderieved = 111;
+    derieved->display();
+
     return 0;
 }
